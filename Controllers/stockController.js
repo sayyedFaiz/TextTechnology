690b89aa300d77a4129e8350e4cc051dd108b190
sayyedFaiz <sayyedfaiz7@gmail.com>
2023-06-14 19:06:16 +0200

    Updated database function

--- Controllers/stockController.js
+++ Controllers/stockController.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const Stock = require("../models/stock");
-
+const convert = require("xml-js");
 const stockController = {
   async fetchStock(req, res) {
     const stockName = req.query.name;
@@ -11,29 +11,38 @@ const stockController = {
       const data = response.data;
       if (data.quotes.length > 0) {
         const symbol = data.quotes[0].symbol;
+        console.log("symbol:", symbol);
         const quoteResponse = await axios.get(
           `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?modules=price`
         );
+
         const quoteData = quoteResponse.data.quoteSummary.result[0].price;
-        const stockDetails = {
-          symbol: quoteData.symbol,
-          price: quoteData.regularMarketPrice.raw,
-          currency: quoteData.currency,
-          dayHigh: quoteData.regularMarketDayHigh.raw,
-          dayLow: quoteData.regularMarketDayLow.raw,
-        };
         // Save the stock details to the database if it doesn't already exist
-        const existingStock = await Stock.findOne({
-          symbol: stockDetails.symbol,
-        });
-
-        if (!existingStock) {
-          const stock = new Stock(stockDetails);
-          await stock.save();
+        let stock = await Stock.findOne({ symbol });
+        if (stock != null) {
+          stock.symbol = quoteData.symbol;
+          stock.price = quoteData.regularMarketPrice.raw;
+          stock.currency = quoteData.currency;
+          stock.dayHigh = quoteData.regularMarketDayHigh.raw;
+          stock.dayLow = quoteData.regularMarketDayLow.raw;
+        } else {
+          stock = new Stock({
+            symbol: quoteData.symbol,
+            price: quoteData.regularMarketPrice.raw,
+            currency: quoteData.currency,
+            dayHigh: quoteData.regularMarketDayHigh.raw,
+            dayLow: quoteData.regularMarketDayLow.raw,
+          });
         }
-        const savedStocks = await Stock.find({});
-
-        res.render("stock", { stockDetails, savedStocks });
+        await stock.save();
+        // console.log(stock);
+        //Convert the response to JSON string and then to XML
+        var options = { compact: true, ignoreComment: true, spaces: 4 };
+        const xml = convert.json2xml(
+          JSON.parse(JSON.stringify(stock)),
+          options
+        );
+        res.render("stock", { stock });
       } else {
         res.render("error");
       }
